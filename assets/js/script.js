5466a54fa3f37d0a276e4fbec92716757470a794
NewtonOmbese <newtonombese1@gmail.com>
2022-06-29 23:30:07 +0300

    giving life to my js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-let amountLoaning = document.getElementById("amountLoan")
+let amountLoaning = document.getElementById("amountLoan");
+let interest = document.getElementById("interestRate");
+let duration = document.getElementById("durationInMonths")
