d4c265caf90f253ef58a7af9028c25053352c3f1
NewtonOmbese <newtonombese1@gmail.com>
2022-06-30 09:35:35 +0300

    adding dom manip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 let amountLoaning = document.getElementById("amountLoan");
 let interest = document.getElementById("interestRate");
 let duration = document.getElementById("durationInMonths")
+let computedSum = document.getElementById("calculateTotal")
+
+computedSum.addEventListener("click", ()=>{
+    
+});
