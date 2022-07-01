11acf4d6d3ef9a8e03a7b18a9e1d8166f0612b4b
NewtonOmbese <newtonombese1@gmail.com>
2022-07-01 20:52:19 +0300

    making it work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,16 @@ let amountLoaning = document.getElementById("amountLoan");
 let interest = document.getElementById("interestRate");
 let duration = document.getElementById("durationInMonths")
 let computedSum = document.getElementById("calculateTotal")
+let accumulatedAmount;
+let totalAmount;
 
-computedSum.addEventListener("click", ()=>{
-    
-});
+computedSum.addEventListener("click", dhafuYetu)
+
+function dhafuYetu(){
+    let toBePaid = amountLoaning.value
+    let time = duration.value
+    let interestPer = interest.value
+    // accumulatedAmount = (interestPer * toBePaid)/100
+    accumulatedAmount = (toBePaid * time * interestPer)/100;
+    console.log(parseInt(accumulatedAmount))
+}
