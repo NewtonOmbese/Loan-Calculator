11850dfc2980be822418ffd23db78dcb2dc46c99
NewtonOmbese <newtonombese1@gmail.com>
2022-07-01 21:15:44 +0300

    working but needs to be resetting to default after calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let amountLoaning = document.getElementById("amountLoan");
 let interest = document.getElementById("interestRate");
 let duration = document.getElementById("durationInMonths");
 let computedSum = document.getElementById("calculateTotal");
-let appendedValue = document.getElementById("value")
+let appendedValue = document.getElementById("value");
 let accumulatedAmount;
 let totalAmount;
 
@@ -12,9 +12,12 @@ function dhafuYetu(){
     let toBePaid = amountLoaning.value
     let time = duration.value
     let interestPer = interest.value
-    // accumulatedAmount = (interestPer * toBePaid)/100
     accumulatedAmount = (toBePaid * time * interestPer)/100;
     // console.log(parseInt(accumulatedAmount))
     totalAmount = parseInt(accumulatedAmount) + parseInt(toBePaid)
     console.log(totalAmount)
+    // totalAmount.innerHTML = appendedValue
+    appendedValue.append(totalAmount)
+    appendedValue.textContent
+    appendedValue.reset()
 }
